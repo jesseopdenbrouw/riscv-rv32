4f8e775fd9716989f0c62479a78292b1304d2365
Jesse op den Brouw <51508208+jesseopdenbrouw@users.noreply.github.com>
2023-01-14 09:44:01 +0100

    Added defines

--- sw/include/uart.h
+++ sw/include/uart.h
@@ -2,8 +2,8 @@
  * uart.h -- definitions for the UARTs
  */
 
-#ifndef _uart1_H
-#define _uart1_H
+#ifndef _UART_H
+#define _UART_H
 
 #include <stdint.h>
 
@@ -27,6 +27,25 @@ int uart1_gets(char buffer[], int size);
 int uart1_printf(const char *format, ...);
 
 #define UART_PRESCALER(A) ((F_CPU)/(A)-1)
+
+#define UART_STOP1 (0 << 0)
+#define UART_STOP2 (1 << 0)
+#define UART_SIZE7 (3 << 2)
+#define UART_SIZE8 (0 << 2)
+#define UART_SIZE9 (2 << 2)
+#define UART_PARITY_NONE (0 << 4)
+#define UART_PARITY_EVEN (2 << 4)
+#define UART_PARITY_ODD (3 << 4)
+#define UART_RCIE (1 << 6)
+#define UART_TCIE (1 << 7)
+
+#define UART_FE (1 << 0)
+#define UART_RF (1 << 1)
+#define UART_RC (1 << 2)
+#define UART_PE (1 << 3)
+#define UART_TC (1 << 4)
+
+#define UART_CTRL_NONE (0)
 
 #ifdef __cplusplus
 }
