5ecf25d9355da385179d000cfef19547c00d7211
Jesse op den Brouw <51508208+jesseopdenbrouw@users.noreply.github.com>
2023-01-14 09:43:46 +0100

    Conditional compilation

--- sw/include/thuasrv32.h
+++ sw/include/thuasrv32.h
@@ -1,7 +1,19 @@
 
+/*
+ * thuasev32.h - include all header files
+ *
+ */
+
+#ifndef _THUASRV32_H
+#define _THUASRV32_H
+
 #include <io.h>
 #include <uart.h>
 #include <syscalls.h>
 #include <csr.h>
 #include <interrupt.h>
+#include <util.h>
+#include <spi.h>
+
+#endif
 
