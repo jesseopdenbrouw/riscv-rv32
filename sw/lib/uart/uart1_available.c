f35a2991394765be2dc2210f135899c41fcef18d
Jesse op den Brouw <51508208+jesseopdenbrouw@users.noreply.github.com>
2023-01-21 12:36:30 +0100

    Added comment

--- sw/lib/uart/uart1_available.c
+++ sw/lib/uart/uart1_available.c
@@ -3,8 +3,7 @@
 #include <ctype.h>
 
 /* THUASRV32 */
-#include "io.h"
-#include "uart.h"
+#include <thuasrv32.h>
 
 /* Frequency of the DE0-CV board */
 #ifndef F_CPU
@@ -15,7 +14,13 @@
 #define BAUD_RATE (9600UL)
 #endif
 
-/* Check if a character is available */
+/**
+ * @file uart1_available.c
+ * @brief Checks if a character is available on UART1
+ *
+ * @param[in] -
+ * @param[out] int == 0 if not available, != 0 if available
+ */
 int uart1_available(void)
 {
 	return (UART1->STAT & 0x04);
