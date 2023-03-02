0ea59007201d73a71696aa0b9c4d286250c664d3
Jesse op den Brouw <51508208+jesseopdenbrouw@users.noreply.github.com>
2023-03-02 21:04:59 +0100

    Update i2c1_transmit_byte.c

--- sw/lib/i2c/i2c1_transmit_byte.c
+++ sw/lib/i2c/i2c1_transmit_byte.c
@@ -6,7 +6,7 @@ uint32_t i2c1_transmit_byte(uint8_t data)
 	I2C1->DATA = data;
 
 	/* Wait for transmission to end */
-	while ((I2C1->STAT & 0x08) == 0x00);
+	while ((I2C1->STAT & I2C_TC) == 0x00);
 
-	return I2C1->STAT & (1 << 5);
+	return I2C1->STAT & I2C_AF;
 } 
