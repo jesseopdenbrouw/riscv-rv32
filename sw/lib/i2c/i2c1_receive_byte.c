6cad28bc06027bdd1d27fab10f3fbc820ac70397
Jesse op den Brouw <51508208+jesseopdenbrouw@users.noreply.github.com>
2023-03-02 21:04:39 +0100

    Update i2c1_receive_byte.c

--- sw/lib/i2c/i2c1_receive_byte.c
+++ sw/lib/i2c/i2c1_receive_byte.c
@@ -2,8 +2,11 @@
 
 uint8_t i2c1_receive_byte(void)
 {
+	/* Send dummy byte */
+	I2C1->DATA = 0xff;
+
 	/* Wait for data transmission completed */
-	while ((I2C1->STAT & 0x10) == 0x00);
+	while ((I2C1->STAT & I2C_TC) == 0x00);
 
 	return I2C1->DATA;
 } 
