5f244aa7cf773476b586a8e5aa77c01a61a09cfc
Jesse op den Brouw <51508208+jesseopdenbrouw@users.noreply.github.com>
2023-03-02 21:01:28 +0100

    Added #defines for bit positions

--- sw/include/i2c.h
+++ sw/include/i2c.h
@@ -25,12 +25,22 @@ uint32_t i2c1_transmit(uint8_t address, uint8_t *buf, uint32_t len);
 uint8_t i2c1_receive_byte(void);
 /* Receive a buffer of uint8_t from I2C1 */
 uint32_t i2c1_receive(uint8_t address, uint8_t *buf, uint32_t len);
+/* Clear the STAT register */
+void i2c1_clearstat(void);
 
 /* Due to rounding toward 0, some speeds may be a bit to high */
 #define I2C_PRESCALER_FM(A) (((A/3UL/400000UL)-1) << 16)
 #define I2C_PRESCALER_SM(A) (((A/2UL/100000UL)-1) << 16)
 #define I2C_FAST_MODE (1 << 2)
 #define I2C_STANDARD_MODE (0 << 2)
+#define I2C_MACK (1 << 11)
+#define I2C_START (1 << 9)
+#define I2C_STOP (1 << 8)
+#define I2C_AF (1 << 5)
+#define I2C_TC (1 << 3)
+#define I2C_READ (1)
+#define I2C_WRITE (0)
+
 
 #ifdef __cplusplus
 }
