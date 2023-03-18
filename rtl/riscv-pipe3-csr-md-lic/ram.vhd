c7f409d85db9a62725a5e4a78b10887438b606b2
Jesse op den Brouw <51508208+jesseopdenbrouw@users.noreply.github.com>
2023-03-18 18:07:04 +0100

    Renamed I_address to I_memaddress,  I_size to I_memsize, I_wrram to I_wren, changes to variable identifiers

--- rtl/riscv-pipe3-csr-md-lic/ram.vhd
+++ rtl/riscv-pipe3-csr-md-lic/ram.vhd
@@ -50,10 +50,10 @@ use work.processor_common_rom.all;
 entity ram is
     port (I_clk : in std_logic;
           I_areset : in std_logic;
-          I_address : in data_type;
+          I_memaddress : in data_type;
+          I_memsize : in memsize_type;
           I_csram : in std_logic;
-          I_size : in memsize_type;
-          I_wrram : in std_logic;
+          I_wren : in std_logic;
           I_datain : in data_type;
           O_dataout : out data_type;
           O_load_misaligned_error : out std_logic;
@@ -74,108 +74,108 @@ begin
     -- RAM + Input & output recoding
     -- The RAM is 32 bits, Big Endian, so we have to recode the inputs
     -- to support Little Endian
-    process (I_clk, I_areset, I_address, I_size, I_wrram, I_csram, I_datain) is
-    variable address_var : integer range 0 to ram_size-1;
+    process (I_clk, I_areset, I_memaddress, I_memsize, I_wren, I_csram, I_datain) is
+    variable address_v : integer range 0 to ram_size-1;
     constant x : std_logic_vector(7 downto 0) := (others => '-');
-    variable datawrite : data_type;
-    variable byteena_var : std_logic_vector(3 downto 0);
-    variable dataout_var : data_type;
+    variable datawrite_v : data_type;
+    variable byteena_v : std_logic_vector(3 downto 0);
+    variable dataout_v : data_type;
     begin
         -- Need only the upper bits for address, the lower two bits select word, halfword or byte
-        address_var := to_integer(unsigned(I_address(ram_size_bits-1 downto 2)));
+        address_v := to_integer(unsigned(I_memaddress(ram_size_bits-1 downto 2)));
         -- Data to write
-        datawrite := I_datain;
+        datawrite_v := I_datain;
         
         -- Clear write bytes
-        byteena_var := "0000";
+        byteena_v := "0000";
 
         -- Reset store misaligned
         O_store_misaligned_error <= '0';
         
          -- Input recoding
-        if I_csram = '1' and I_wrram = '1' then
-            case I_size is
+        if I_csram = '1' and I_wren = '1' then
+            case I_memsize is
                 -- Byte size
                 when memsize_byte =>
-                    case I_address(1 downto 0) is
-                        when "00" => datawrite := datawrite(7 downto 0) & x & x & x; byteena_var := "1000";
-                        when "01" => datawrite := x & datawrite(7 downto 0) & x & x; byteena_var := "0100";
-                        when "10" => datawrite := x & x & datawrite(7 downto 0) & x; byteena_var := "0010";
-                        when "11" => datawrite := x & x & x & datawrite(7 downto 0); byteena_var := "0001";
-                        when others => datawrite := x & x & x & x; O_store_misaligned_error <= '1';
+                    case I_memaddress(1 downto 0) is
+                        when "00" => datawrite_v := datawrite_v(7 downto 0) & x & x & x; byteena_v := "1000";
+                        when "01" => datawrite_v := x & datawrite_v(7 downto 0) & x & x; byteena_v := "0100";
+                        when "10" => datawrite_v := x & x & datawrite_v(7 downto 0) & x; byteena_v := "0010";
+                        when "11" => datawrite_v := x & x & x & datawrite_v(7 downto 0); byteena_v := "0001";
+                        when others => datawrite_v := x & x & x & x; O_store_misaligned_error <= '1';
                     end case;
                 -- Half word size, on 2-byte boundaries
                 when memsize_halfword =>
-                    if I_address(1 downto 0) = "00" then
-                        datawrite := datawrite(7 downto 0) & datawrite(15 downto 8) & x & x;
-                        byteena_var := "1100";
-                    elsif I_address(1 downto 0) = "10" then
-                        datawrite := x & x & datawrite(7 downto 0) & datawrite(15 downto 8);
-                        byteena_var := "0011";
+                    if I_memaddress(1 downto 0) = "00" then
+                        datawrite_v := datawrite_v(7 downto 0) & datawrite_v(15 downto 8) & x & x;
+                        byteena_v := "1100";
+                    elsif I_memaddress(1 downto 0) = "10" then
+                        datawrite_v := x & x & datawrite_v(7 downto 0) & datawrite_v(15 downto 8);
+                        byteena_v := "0011";
                     else
-                        datawrite :=  x & x & x & x; O_store_misaligned_error <= '1';
+                        datawrite_v :=  x & x & x & x; O_store_misaligned_error <= '1';
                     end if;
                 -- Word size, on 4-byte boundaries
                 when memsize_word =>
-                    if I_address(1 downto 0) = "00" then
-                        datawrite := datawrite(7 downto 0) & datawrite(15 downto 8) & datawrite(23 downto 16) & datawrite(31 downto 24);
-                        byteena_var := "1111";
+                    if I_memaddress(1 downto 0) = "00" then
+                        datawrite_v := datawrite_v(7 downto 0) & datawrite_v(15 downto 8) & datawrite_v(23 downto 16) & datawrite_v(31 downto 24);
+                        byteena_v := "1111";
                     else
-                        datawrite :=  x & x & x & x; O_store_misaligned_error <= '1';
+                        datawrite_v :=  x & x & x & x; O_store_misaligned_error <= '1';
                     end if;
                 when others =>
-                    datawrite := x & x & x & x;
+                    datawrite_v := x & x & x & x;
             end case;
         else
-            datawrite := x & x & x & x;
+            datawrite_v := x & x & x & x;
         end if;
        
         -- The RAM itself
         if rising_edge(I_clk) then
             -- Write to RAM
             -- ramll is byte y, ramlh is byte y+1, ramhl is byte y+2, ramhh is byte y+3
-            if byteena_var(3) = '1' then
-                ramhh(address_var) <= datawrite(31 downto 24);
+            if byteena_v(3) = '1' then
+                ramhh(address_v) <= datawrite_v(31 downto 24);
             end if;
-            if byteena_var(2) = '1' then
-                ramhl(address_var) <= datawrite(23 downto 16);
+            if byteena_v(2) = '1' then
+                ramhl(address_v) <= datawrite_v(23 downto 16);
             end if;
-            if byteena_var(1) = '1' then
-                ramlh(address_var) <= datawrite(15 downto 8);
+            if byteena_v(1) = '1' then
+                ramlh(address_v) <= datawrite_v(15 downto 8);
             end if;
-            if byteena_var(0) = '1' then
-                ramll(address_var) <= datawrite(7 downto 0);
+            if byteena_v(0) = '1' then
+                ramll(address_v) <= datawrite_v(7 downto 0);
             end if;
             -- Read from RAM, in Big Endian format (31-24, 23-16, 15-8, 7-0)
-            dataout_var := ramhh(address_var) & ramhl(address_var) & ramlh(address_var) & ramll(address_var);
+            dataout_v := ramhh(address_v) & ramhl(address_v) & ramlh(address_v) & ramll(address_v);
         end if;
 
         O_load_misaligned_error <= '0';
         -- Output recoding
         if I_csram = '1' then
-            case I_size is
+            case I_memsize is
                 -- Byte size
                 when memsize_byte =>
-                    case I_address(1 downto 0) is
-                        when "00" => O_dataout <= x & x & x & dataout_var(31 downto 24);
-                        when "01" => O_dataout <= x & x & x & dataout_var(23 downto 16);
-                        when "10" => O_dataout <= x & x & x & dataout_var(15 downto 8);
-                        when "11" => O_dataout <= x & x & x & dataout_var(7 downto 0);
+                    case I_memaddress(1 downto 0) is
+                        when "00" => O_dataout <= x & x & x & dataout_v(31 downto 24);
+                        when "01" => O_dataout <= x & x & x & dataout_v(23 downto 16);
+                        when "10" => O_dataout <= x & x & x & dataout_v(15 downto 8);
+                        when "11" => O_dataout <= x & x & x & dataout_v(7 downto 0);
                         when others => O_dataout <= x & x & x & x; O_load_misaligned_error <= '1';
                     end case;
                 -- Half word size
                 when memsize_halfword =>
-                    if I_address(1 downto 0) = "00" then
-                        O_dataout <= x & x & dataout_var(23 downto 16) & dataout_var(31 downto 24);
-                    elsif I_address(1 downto 0) = "10" then
-                        O_dataout <= x & x & dataout_var(7 downto 0) & dataout_var(15 downto 8);
+                    if I_memaddress(1 downto 0) = "00" then
+                        O_dataout <= x & x & dataout_v(23 downto 16) & dataout_v(31 downto 24);
+                    elsif I_memaddress(1 downto 0) = "10" then
+                        O_dataout <= x & x & dataout_v(7 downto 0) & dataout_v(15 downto 8);
                     else
                         O_dataout <= x & x & x & x; O_load_misaligned_error <= '1';
                     end if;
                 -- Word size
                 when memsize_word =>
-                    if I_address(1 downto 0) = "00" then
-                        O_dataout <= dataout_var(7 downto 0) & dataout_var(15 downto 8) & dataout_var(23 downto 16) & dataout_var(31 downto 24);
+                    if I_memaddress(1 downto 0) = "00" then
+                        O_dataout <= dataout_v(7 downto 0) & dataout_v(15 downto 8) & dataout_v(23 downto 16) & dataout_v(31 downto 24);
                     else
                         O_dataout <= x & x & x & x; O_load_misaligned_error <= '1';
                     end if;
@@ -199,4 +199,3 @@ begin
     -- synthesis translate_on
 
 end architecture rtl;
-
