26fefa5897b05a056346c84a71ca2e6a15e16334
Jesse op den Brouw <51508208+jesseopdenbrouw@users.noreply.github.com>
2023-03-18 18:08:53 +0100

    Renamed I_address to I_memaddress, I_size to I_memsize, changed variable names

--- rtl/riscv-pipe3-csr-md-lic/rom.vhd
+++ rtl/riscv-pipe3-csr-md-lic/rom.vhd
@@ -55,14 +55,14 @@ entity rom is
     port (I_clk : in std_logic;
           I_areset : in std_logic;
           I_pc : in data_type;
-          I_address : in data_type;
+          I_memaddress : in data_type;
+          I_memsize : in memsize_type;
           I_csrom : in std_logic;
           I_wren : in std_logic;
-          I_size : in memsize_type;
           I_stall : in std_logic;
           O_instr : out data_type;
           I_datain : in data_type;
-          O_data_out : out data_type;
+          O_dataout : out data_type;
           --
           O_instruction_misaligned_error : out std_logic;
           O_load_misaligned_error : out std_logic;
@@ -80,20 +80,20 @@ begin
     O_instruction_misaligned_error <= '0' when I_pc(1 downto 0) = "00" else '1';        
 
     -- ROM, for both instructions and read-write data
-    process (I_clk, I_areset, I_pc, I_address, I_csrom, I_size, I_wren, I_datain) is
-    variable address_instr : integer range 0 to rom_size-1;
-    variable address_data : integer range 0 to rom_size-1;
-    variable instr_var : data_type;
-    variable instr_recode : data_type;
-    variable romdata_var : data_type;
+    process (I_clk, I_areset, I_pc, I_memaddress, I_csrom, I_memsize, I_wren, I_datain) is
+    variable address_instr_v : integer range 0 to rom_size-1;
+    variable address_data_v : integer range 0 to rom_size-1;
+    variable instr_v : data_type;
+    variable instr_recode_v : data_type;
+    variable romdata_v : data_type;
     constant x : std_logic_vector(7 downto 0) := (others => 'X');
     begin
         -- Calculate addresses
-        address_instr := to_integer(unsigned(I_pc(rom_size_bits-1 downto 2)));
-        address_data := to_integer(unsigned(I_address(rom_size_bits-1 downto 2)));
+        address_instr_v := to_integer(unsigned(I_pc(rom_size_bits-1 downto 2)));
+        address_data_v := to_integer(unsigned(I_memaddress(rom_size_bits-1 downto 2)));
  
         -- Set store misaligned error
-        if I_csrom = '1' and I_wren = '1' and I_size /= memsize_word then
+        if I_csrom = '1' and I_wren = '1' and I_memsize /= memsize_word then
             O_store_misaligned_error <= '1';
         else
             O_store_misaligned_error <= '0';
@@ -104,47 +104,47 @@ begin
         if rising_edge(I_clk) then
             -- Read the instruction
             if I_stall = '0' then
-                instr_var := rom(address_instr);
+                instr_v := rom(address_instr_v);
             end if;
             -- Read the data
-            romdata_var := rom(address_data);
+            romdata_v := rom(address_data_v);
             if HAVE_BOOTLOADER_ROM then
                 -- Write the ROM ;-)
-                if I_csrom = '1' and I_wren = '1' and I_size = memsize_word then
-                    rom(address_data) <= I_datain(7 downto 0) & I_datain(15 downto 8) & I_datain(23 downto 16) & I_datain(31 downto 24);
+                if I_csrom = '1' and I_wren = '1' and I_memsize = memsize_word then
+                    rom(address_data_v) <= I_datain(7 downto 0) & I_datain(15 downto 8) & I_datain(23 downto 16) & I_datain(31 downto 24);
                 end if;
             end if;
         end if;
 
         -- Recode instruction
-        O_instr <= instr_var(7 downto 0) & instr_var(15 downto 8) & instr_var(23 downto 16) & instr_var(31 downto 24);
+        O_instr <= instr_v(7 downto 0) & instr_v(15 downto 8) & instr_v(23 downto 16) & instr_v(31 downto 24);
         
         O_load_misaligned_error <= '0';
         
         -- By natural size, for data
         if I_csrom = '1' then
-            if I_size = memsize_word and I_address(1 downto 0) = "00" then
-                O_data_out <= romdata_var(7 downto 0) & romdata_var(15 downto 8) & romdata_var(23 downto 16) & romdata_var(31 downto 24);
-            elsif I_size = memsize_halfword and I_address(1 downto 0) = "00" then
-                O_data_out <= x & x & romdata_var(23 downto 16) & romdata_var(31 downto 24);
-            elsif I_size = memsize_halfword and I_address(1 downto 0) = "10" then
-                O_data_out <= x & x & romdata_var(7 downto 0) & romdata_var(15 downto 8);
-            elsif I_size = memsize_byte then
-                case I_address(1 downto 0) is
-                    when "00" => O_data_out <= x & x & x & romdata_var(31 downto 24);
-                    when "01" => O_data_out <= x & x & x & romdata_var(23 downto 16);
-                    when "10" => O_data_out <= x & x & x & romdata_var(15 downto 8);
-                    when "11" => O_data_out <= x & x & x & romdata_var(7 downto 0);
-                    when others => O_data_out <= x & x & x & x; O_load_misaligned_error <= '1';
+            if I_memsize = memsize_word and I_memaddress(1 downto 0) = "00" then
+                O_dataout <= romdata_v(7 downto 0) & romdata_v(15 downto 8) & romdata_v(23 downto 16) & romdata_v(31 downto 24);
+            elsif I_memsize = memsize_halfword and I_memaddress(1 downto 0) = "00" then
+                O_dataout <= x & x & romdata_v(23 downto 16) & romdata_v(31 downto 24);
+            elsif I_memsize = memsize_halfword and I_memaddress(1 downto 0) = "10" then
+                O_dataout <= x & x & romdata_v(7 downto 0) & romdata_v(15 downto 8);
+            elsif I_memsize = memsize_byte then
+                case I_memaddress(1 downto 0) is
+                    when "00" => O_dataout <= x & x & x & romdata_v(31 downto 24);
+                    when "01" => O_dataout <= x & x & x & romdata_v(23 downto 16);
+                    when "10" => O_dataout <= x & x & x & romdata_v(15 downto 8);
+                    when "11" => O_dataout <= x & x & x & romdata_v(7 downto 0);
+                    when others => O_dataout <= x & x & x & x; O_load_misaligned_error <= '1';
                 end case;
             else
                 -- Chip select, but not aligned
-                O_data_out <= x & x & x & x;
+                O_dataout <= x & x & x & x;
                 O_load_misaligned_error <= '1';
             end if;
         else
             -- No chip select, so no data
-            O_data_out <= x & x & x & x;
+            O_dataout <= x & x & x & x;
         end if;
     end process;
 
