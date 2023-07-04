3fcfc8c548e00c9a897defe55436df66ba9a4136
Jesse op den Brouw <51508208+jesseopdenbrouw@users.noreply.github.com>
2023-07-04 09:31:37 +0200

    Use snprintf to print out information

--- sw/ctor_c/ctor_c.c
+++ sw/ctor_c/ctor_c.c
@@ -33,21 +33,29 @@ extern func_ptr_t __fini_array_start[1], __fini_array_end[1];
 
 int main(int argc, char *argv[], char *env[])
 {
+	char buffer[128];
+
 	uart1_init(BAUD_RATE, UART_CTRL_NONE);
 
 	uart1_puts("From linker symbols:\r\n");
-	uart1_printf(">> %08x %p\r\n", *__init_array_start, __init_array_start);
-	uart1_printf(">> %08x %p\r\n", *__init_array_end, __init_array_end);
-	uart1_printf(">> %08x %p\r\n", *__fini_array_start, __fini_array_start);
-	uart1_printf(">> %08x %p\r\n", *__fini_array_end, __fini_array_end);
+	snprintf(buffer, sizeof buffer, ">> %08lx %p\r\n", (uint32_t) *__init_array_start, __init_array_start);
+	uart1_puts(buffer);
+	snprintf(buffer, sizeof buffer, ">> %08lx %p\r\n", (uint32_t) *__init_array_end, __init_array_end);
+	uart1_puts(buffer);
+	snprintf(buffer, sizeof buffer, ">> %08lx %p\r\n", (uint32_t) *__fini_array_start, __fini_array_start);
+	uart1_puts(buffer);
+	snprintf(buffer, sizeof buffer, ">> %08lx %p\r\n", (uint32_t) *__fini_array_end, __fini_array_end);
+	uart1_puts(buffer);
 
 	uart1_puts("Constructors:\r\n");
 	for ( func_ptr_t* func = __init_array_start; func != __init_array_end; func++ ) {
-		uart1_printf("-> Function address: %p, @%p\r\n", *func, func);
+		snprintf(buffer, sizeof buffer, "-> Function address: %p, @%p\r\n", *func, func);
+		uart1_puts(buffer);
 	}
 
 	uart1_puts("Destructors:\r\n");
 	for ( func_ptr_t* func = __fini_array_start; func != __fini_array_end; func++ ) {
-		uart1_printf("-> Function address: %p, @%p\r\n", *func, func);
+		snprintf(buffer, sizeof buffer, "-> Function address: %p, @%p\r\n", *func, func);
+		uart1_puts(buffer);
 	}
 }
